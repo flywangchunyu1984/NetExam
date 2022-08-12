c55915508ac50b6c5c23e24834896c9d9561329e
saika-japan <mk1332jp@gmail.com>
2022-08-12 20:34:00 +0900

    画面文言修正

--- exam/src/components/student/answerScore.vue
+++ exam/src/components/student/answerScore.vue
@@ -1,31 +1,30 @@
 <template>
   <div class="score">
     <div class="title">
-      <p class="name">计算机网络</p>
-      <p class="description">(总分：100分,限时：100分钟)</p>
-      <p class="description">学生：大咸鱼</p>
+      <p class="name">セキュリティ研修テスト</p>
+      <p class="description">(満点：100点)</p>
     </div>
     <div class="total">
       <div class="look">
-        本次考试成绩
+        試験結果
       </div>
       <div class="show">
         <div class="img1" :class="{'img1Transform': imgShow}">
-          <img :src="imgSrc.fail1" alt="哭了" v-if="score < 60">
-          <img :src="imgSrc.pass1" alt="过了" v-if="score >= 60">
+          <img :src="imgSrc.fail1" alt="不合格" v-if="score < 60">
+          <img :src="imgSrc.pass1" alt="合格" v-if="score >= 60">
         </div>
         <div class="number" :class="{'border': isTransition}">
           <span>{{score}}</span>
-          <span>分数</span>
+          <span>得点</span>
         </div>
         <div class="img2" :class="{'img2Transform': imgShow}">
-          <img :src="imgSrc.fail2" alt="哭了" v-if="score < 60">
-          <img :src="imgSrc.pass2" alt="过了" v-if="score >= 60">
+          <img :src="imgSrc.fail2" alt="不合格" v-if="score < 60">
+          <img :src="imgSrc.pass2" alt="合格" v-if="score >= 60">
         </div>
       </div>
       <ul class="time">
-        <li class="start"><span>开始时间</span> <span>{{startTime}}</span></li>
-        <li class="end"><span>结束时间</span> <span>{{endTime}}</span></li>
+        <li class="start"><span>開始時刻</span> <span>{{startTime}}</span></li>
+        <li class="end"><span>終了時刻</span> <span>{{endTime}}</span></li>
       </ul>
     </div>
   </div>
